42dda1a21a4a600a817b620fc9d9c3bc050e0b0e
krunom01 <krunom92@gmail.com>
2019-02-04 18:29:49 +0100

    on delete cascade mysql

--- script.sql
+++ script.sql
@@ -7,15 +7,16 @@ id int not null primary key auto_increment,
 content text,
 cr_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 image LONGBLOB
-);
+)engine=InnoDB;
 
 create table comment (
 id int not null primary key auto_increment,
 content text not null,
 cr_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 id_post int,
-foreign key (id_post) references post(id) ON DELETE CASCADE
-);
+foreign key (id_post)
+references post(id) ON DELETE CASCADE
+)engine=InnoDB;
 
 
 insert into post (content) values ('Evo danas pada kiša opet :('), ('Jedem jagode.'),('Jedesdfm jagode.');
